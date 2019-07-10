d8e0aea4c67fafc15121dd41ffbe37d000179d08
tchapi <regbasket+github@gmail.com>
2019-07-10 11:37:20 +0200

    Fix database init

--- scripts/db/databases.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE IF NOT EXISTS `standardfile`;
-CREATE USER 'standardfile'@'%' IDENTIFIED BY 'test';
-GRANT ALL ON `standardfile`.* TO 'standardfile'@'%';
-
-CREATE DATABASE IF NOT EXISTS `passbolt`;
-CREATE USER 'passbolt'@'%' IDENTIFIED BY 'test';
-GRANT ALL ON `passbolt`.* TO 'passbolt'@'%';
