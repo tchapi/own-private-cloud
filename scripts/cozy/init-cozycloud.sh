29ad1342e0122b135f064e7295afbf562e5ac7d4
tchapi <regbasket+github@gmail.com>
2019-07-14 23:21:04 +0200

    Fix env vars, add warning in env file
    Make scripts runnable

--- scripts/cozy/init-cozycloud.sh
+++ scripts/cozy/init-cozycloud.sh
@@ -4,6 +4,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source $DIR/../../environment/production.env
 
 echo "### Creating cloud instance"
-docker-compose run --rm --entrypoint "export COZY_ADMIN_PASSWORD=${COZY_ADMIN_PASSPHRASE}" certbot
-docker-compose run --rm --entrypoint "\
-  cozy-stack instances add --apps contacts,drive,photos,settings ${CLOUD_DOMAIN} --email ${EMAIL} --passphrase ${CLOUD_MASTER_PASSWORD}" certbot
+docker exec -it cozy bash -c '\
+  export COZY_ADMIN_PASSWORD=${COZY_ADMIN_PASSPHRASE} && \
+  cozy-stack instances add --apps contacts,drive,photos,settings ${CLOUD_DOMAIN} --email ${EMAIL} --passphrase ${CLOUD_MASTER_PASSWORD}'
