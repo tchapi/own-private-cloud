b30882420f319ec0740a266285bd863ef5c458d4
Cyril <cyril@joshfire.com>
2019-12-05 16:48:18 +0100

    Rename USER → PASSBOLT_USER, fix comments

--- scripts/passbolt/init-admin-user.sh
+++ scripts/passbolt/init-admin-user.sh
@@ -4,4 +4,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source $DIR/../../.env
 
 echo "### Creating first admin user"
-docker exec passbolt su -m -c "/var/www/passbolt/bin/cake passbolt register_user -u ${EMAIL} -f ${USER} -l ${USER} -r admin" -s /bin/sh www-data
+docker exec passbolt su -m -c "/var/www/passbolt/bin/cake passbolt register_user -u ${EMAIL} -f ${PASSBOLT_USER} -l ${PASSBOLT_USER} -r admin" -s /bin/sh www-data
