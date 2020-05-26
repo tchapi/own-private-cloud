4023ead2cf0fd1d3f49bb74af601a940848aeaba
tchapi <regbasket+github@gmail.com>
2020-05-26 23:18:02 +0200

    Add Cryptpad (WIP)

---
+++ configurations/cryptpad/application_config.js
@@ -0,0 +1,16 @@
+/*
+ * You can override the configurable values from this file.
+ * The recommended method is to make a copy of this file (/customize.dist/application_config.js)
+   in a 'customize' directory (/customize/application_config.js).
+ * If you want to check all the configurable values, you can open the internal configuration file
+   but you should not change it directly (/common/application_config_internal.js)
+*/
+define(['/common/application_config_internal.js'], function (AppConfig) {
+    AppConfig.surveyURL = "";
+    AppConfig.availablePadTypes =  ['drive', 'teams', 'pad', 'sheet', 'code', 'file'];
+    AppConfig.hideLoadingScreenTips = true;
+    AppConfig.availableLanguages = ['en'];
+    AppConfig.disableAnonymousStore = true;
+
+    return AppConfig;
+});
