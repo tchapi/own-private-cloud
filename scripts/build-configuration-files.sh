825be2585b851fe596bc26ea7e1cc2643de3c3e2
tchapi <regbasket+github@gmail.com>
2020-10-26 22:46:55 +0100

    Add index.json repo for SN

--- scripts/build-configuration-files.sh
+++ scripts/build-configuration-files.sh
@@ -26,6 +26,20 @@ expandVars ./configurations/standardnotes/templates/autocomplete-tags.json.templ
 expandVars ./configurations/standardnotes/templates/plus-editor.json.template ./configurations/standardnotes/extensions/plus-editor.json
 expandVars ./configurations/standardnotes/templates/secure-spreadsheets.json.template ./configurations/standardnotes/extensions/secure-spreadsheets.json
 expandVars ./configurations/standardnotes/templates/simple-task-editor.json.template ./configurations/standardnotes/extensions/simple-task-editor.json
+# Create the index.json repo file
+extensions=(./configurations/standardnotes/extensions/*.json)
+total=${#extensions[@]}
+i=0
+cp ./configurations/standardnotes/templates/index.json.template ./configurations/standardnotes/extensions/index.json
+# Quite convoluted, but works ok 🤷‍
+for f in "${extensions[@]}"; do
+  i=$(( i + 1 ))
+  echo "`(($i==$total))`"
+  awk -v ext="%`basename $f`%" -v f="$f" -v fi="$i" -v t="$total" 'NR==FNR { a[n++]=$0; next }
+    $0 ~ ext { for (i=0;i<n -1;++i) print "    "a[i]; if (fi!=t) print "    },"; else print "    }"; next }
+    1' $f ./configurations/standardnotes/extensions/index.json > ./configurations/standardnotes/extensions/index.json.done
+  mv ./configurations/standardnotes/extensions/index.json.done ./configurations/standardnotes/extensions/index.json
+done
 
 # XBS configuration
 expandVars ./configurations/xbs/settings.json.template ./configurations/xbs/settings.json
